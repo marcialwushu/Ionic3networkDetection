07408a84cb69e73de309099ab104983b9e564b70
marcialwushu <marcialwushu@gmail.com>
2018-04-03 10:18:38 -0300

    network changes with connectionStatedisplay message with toastController

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,11 +16,21 @@ export class HomePage {
   ionViewDidEnter(){
    this.network.onConnect().subscribe(data => {
      console.log(data)
+     this.displayNetworkUpdate(data.type);
    }, error => console.log(error));
 
    this.network.onDisconnect().subscribe(data => {
      console.log(data)
+     this.displayNetworkUpdate(data.type);
    }, error => console.log(error));
+  }
+
+  displayNetworkUpdate(connectionState: string){
+    let networkType = this.network.type;
+    this.toast.create({
+      message: `You are now ${connectionState} via ${networkType}`,
+      duration: 3000
+    }).present();
   }
 
 }
